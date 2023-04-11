58e0033473b4b1e48e1b5d7d2d7c0cebfab50e32
Saba1119 <100400271+Saba1119@users.noreply.github.com>
2023-04-11 14:35:50 +0530

    Create main.go

---
+++ myapp/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	 // "github.com/aws/aws-sdk-go/aws"
+	 "github.com/aws/aws-sdk-go/aws/session"
+	// "github.com/aws/aws-sdk-go/service/dynamodb"
+	// "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"gopkg.in/yaml.v2"
+	"my-app/src/db"
+        "my-app/src/s3-old"
+        "my-app/src/ec2-cloudwatch"
+     )
+
+type Config struct {
+	TableName string `yaml:"tableName"`
+	AWS_REGION string `yaml:"AWS_REGION"`
+    BUCKET_NAME string `yaml:"BUCKET_NAME"`
+    KMS_KEY string `yaml:"KMS_KEY"`
+    ImageID            string `yaml:"IMAGE_ID"`
+	InstanceType       string `yaml:"INSTANCE_TYPE"`
+	GroupName          string `yaml:"GROUP_NAME"`
+	AlarmName          string `yaml:"ALARM_NAME"`
+	SecurityGroupName  string `yaml:"SECURITYGROUP_NAME"`
+	VPCID              string `yaml:"VPC_ID"`
+	TargetGroupName1   string `yaml:"TARGETGROUP_NAME1"`
+	TargetGroupName2   string `yaml:"TARGETGROUP_NAME2"`
+	ALBName            string `yaml:"ALB_NAME"`
+	SecurityGroup1FROMPort int64  `yaml:"Sgfrom_PORT1"`
+	SecurityGroup1TOPort int64  `yaml:"Sgto_PORT1"`
+	SecurityGroup2FROMPort int64  `yaml:"Sgfrom_PORT2"`
+	SecurityGroup2TOPort int64  `yaml:"Sgto_PORT2"`
+	TargetGroup1PORT     int64  `yaml:"TG1PORT"`
+	TargetGroup2PORT     int64  `yaml:"TG2PORT"`
+	Listener1PORT        int64  `yaml:"LISTENER1PORT"`
+	Listener2PORT        int64  `yaml:"LISTENER2PORT"`
+
+
+	InstanceID          string `yaml:"INSTANCE_ID"`
+    TargetGroupARN1   string  `yaml:"TARGETGROUP_ARN1"`
+    TargetGroupARN2   string  `yaml:"TARGETGROUP_ARN2"`
+    // KMSID            string `yaml:"KMS_ID"`
+
+}
+
+func main()  {
+	//  Open the configuration file
+    configFile, err := os.Open("config.yaml")
+    if err != nil {
+        fmt.Println("Error opening configuration file:", err)
+        os.Exit(1)
+    }
+    defer configFile.Close()
+
+    // Parse the configuration file
+    config := Config{}
+    err = yaml.NewDecoder(configFile).Decode(&config)
+    if err != nil {
+        fmt.Println("Error parsing configuration file:", err)
+        os.Exit(1)
+    }   
+    sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+    if err != nil {
+        fmt.Println("Error creating session:", err)
+        os.Exit(1)
+     }
+	CreateS3(&config, sess)
+	Createec2(&config, sess)
+	CreateDb(&config, sess)	
+// 	teardown(&config, sess)
+}
